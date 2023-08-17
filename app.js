ce65110ba2df0905cef2fff56efde60c117c1580
terwebs <terwebs88@gmail.com>
2023-08-17 17:51:42 -0600

    add completed behavior and styles

--- app.js
+++ app.js
@@ -1,58 +1,111 @@
 const form = document.querySelector('.form')
-const list = document.querySelector('.list')
+const list = document.querySelector('.list-container')
 const input = document.querySelector('.input-task')
+const completed = document.querySelector(".completed-tasks")
 let id = 0;
 
-const todoArray = [];
+const todoArray = []
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  id++;
+  e.preventDefault()
+  id++
   const todoItem = new Todo(id, input.value)
   todoArray.push(todoItem)
   console.log(todoArray)
   renderTask(todoItem)
   clearInput()
-});
+})
 
 class Todo {
-  constructor(id, task){
-    this.id = id;
-    this.task = task;
+  constructor (id, task) {
+    this.id = id
+    this.task = task
   }
 }
 
+// display the data
 
-//display the data
+function renderTask (todoItem) {
+  const todo = document.createElement('div')
+  const task = document.createElement('p')
+  const checkComplete = document.createElement("input")
+  checkComplete.type = "checkbox"
+  checkComplete.classList.add("todo-item_done")
 
-function renderTask(todoItem){
-    const todo = document.createElement("div")
-    const task = document.createElement("p");
-    task.innerText = todoItem.task;
-    todo.appendChild(task);
+  todo.appendChild(checkComplete)
+  task.innerText = todoItem.task
+  task.classList.add("todo-content")
+  todo.appendChild(task)
 
-    const deleteSpan = document.createElement("span");
-    deleteSpan.classList.add("delete")
-    deleteSpan.innerText = "x"
-    todo.appendChild(deleteSpan)
-    todo.classList.add("todo-item")
-    list.appendChild(todo)
-
+  const deleteSpan = document.createElement('span')
+  deleteSpan.classList.add('delete')
+  deleteSpan.innerText = 'x'
+  todo.appendChild(deleteSpan)
+  todo.classList.add('todo-item')
+  list.appendChild(todo)
 }
 
-
-function clearInput(){
-  input.value = ""
+function clearInput () {
+  input.value = ''
 }
 
-function removeItem(){
-  list.addEventListener("click", (e)=> {
-    if(e.target.classList.contains("delete")){
-
+function removeItem () {
+  list.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
       e.target.parentElement.remove()
-      console.log("removed")
+      console.log('removed')
+    }
+  })
+  completed.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+      e.target.parentElement.remove()
+      console.log('removed')
     }
   })
 }
 
+
+function checkItem () {
+  list.addEventListener('click', (e) => {
+    if (e.target.classList.contains('todo-item_done')) {
+      if (e.target.nextElementSibling.classList.contains("checked")){
+        e.target.nextElementSibling.classList.remove("checked")
+        console.log(e.target.parentElement)
+      } else {
+        e.target.nextElementSibling.classList.add("checked")
+        completed.appendChild(e.target.parentElement)
+        checkCompleteditems()
+      }
+    }
+  })
+}
+
+
+function checkItemCompleted () {
+  completed.addEventListener('click', (e) => {
+    if (e.target.classList.contains('todo-item_done')) {
+      if (e.target.nextElementSibling.classList.contains("checked")){
+        e.target.nextElementSibling.classList.remove("checked")
+        list.appendChild(e.target.parentElement)
+        checkCompleteditems()
+      } else {
+        e.target.nextElementSibling.classList.add("checked")
+      }
+    }
+  })
+}
+
+function checkCompleteditems() {
+  if(completed.childElementCount >0 ){
+    completed.classList.add("show-completed")
+    console.log(completed.childElementCount)
+  } else {
+    completed.classList.remove("show-completed")
+  }
+}
+
 removeItem()
+checkItem ()
+checkItemCompleted()
+
+
